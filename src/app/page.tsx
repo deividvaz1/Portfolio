1e7b9b51b4cb83c5484923e8dce6e5c29402efb9
deividvaz1 <deividvaz100@gmail.com>
2023-06-12 17:38:03 -0300

    separando por partes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,18 @@ export default function Home() {
     <div className="relative h-screen w-full overflow-hidden font-bold text-textColor">
       <main className="mx-auto flex h-full max-w-screen-2xl items-center justify-center">
         <div className="z-50 flex h-[85%] w-[85%] items-start justify-between bg-transparent text-white">
-          <div className="h-96 w-16 bg-bodyColor"></div>
+          <div className="h-96 w-16 bg-bodyColor">
+            <h1>navbar</h1>
+          </div>
           <div className="flex h-full w-[94%] items-center bg-transparent ">
             {/* LEFT part start */}
-            <div className="h-full w-5/12 bg-bodyColor"></div>
+            <div className="h-full w-5/12 bg-bodyColor">
+              <h1>hero</h1>
+            </div>
             {/* RIGHT part start */}
-            <div className="h-[100%] w-8/12 bg-bodyColor"></div>
+            <div className="h-[95%] w-8/12 bg-bodyColor">
+              <h1>teste</h1>
+            </div>
           </div>
         </div>
       </main>
