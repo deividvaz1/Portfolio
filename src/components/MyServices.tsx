cabe61a9638ee974e970f5b04be26f426aa867a0
Deivid Vaz <deividvaz100@gmail.com>
2023-06-13 22:39:27 -0300

    alterações no css

--- src/components/MyServices.tsx
+++ src/components/MyServices.tsx
@@ -24,7 +24,7 @@ export function MyServices() {
       />
       <ServicesCard
         icons={<FaAppStoreIos />}
-        title="SEO"
+        title="Desenvolvedor de Front-End"
         subTitle="lorem ipsum, dolor sit amet consectetur adipiscining elit. Hic, provident. Accusamus at dolorem expedita dicta."
       />
     </div>
