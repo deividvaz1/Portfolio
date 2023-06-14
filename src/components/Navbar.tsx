26bf2aa663b6f555d8027cf28b5ca493ef317a88
deividvaz1 <deividvaz100@gmail.com>
2023-06-14 17:37:09 -0300

    mudanças no css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import { SiGooglechat } from 'react-icons/si'
 export function Navbar() {
   return (
     <div className=" flex h-96 w-16 flex-col gap-4">
-      <div className="group flex h-20 w-full cursor-pointer items-center justify-center rounded-3xl bg-bodyColor">
+      <div className=" group flex h-20 w-full cursor-pointer items-center justify-center rounded-3xl bg-bodyColor ">
         {/* NAVBAR */}
         <div className="flex flex-col gap-1.5 overflow-hidden">
           <span className="inline-block h-[2px] w-8 -translate-x-2 bg-textColor transition-transform duration-300 group-hover:translate-x-0 group-hover:bg-designColor"></span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: "var(--font-roboto)",
-        alt: "var(--font-bai-jamjuree)",
+        sans: 'var(--font-roboto)',
+        alt: 'var(--font-bai-jamjuree)',
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        bodyColor: "#191919",
-        textColor: "rgba(255, 255, 255, 0.85)",
-        designColor: "#edff20",
+        bodyColor: '#191919',
+        textColor: 'rgba(255, 255, 255, 0.85)',
+        designColor: '#00FFFF',
         titleRoundBg:
-          "linear-gradient(135deg,rgba(120, 204, 109,.15) 0%, rgba(120, 204, 109, 1) 100%)",
+          'linear-gradient(135deg,rgba(120, 204, 109,.15) 0%, rgba(120, 204, 109, 1) 100%)',
       },
       animation: {
-        "spin-slow": "spin 15s linear infinite",
-        "reverse-spin": "reverse-spin 15s linear infinite",
+        'spin-slow': 'spin 15s linear infinite',
+        'reverse-spin': 'reverse-spin 15s linear infinite',
       },
       keyframes: {
-        "reverse-spin": {
+        'reverse-spin': {
           from: {
-            transform: "rotate(360deg)",
+            transform: 'rotate(360deg)',
           },
         },
       },
       boxShadow: {
-        greenShadow: "0px 0px 188px -14px rgba(237,255,32,1)",
-        testShadow: "11px 0px 13px -15px rgba(0,0,0,1)",
+        greenShadow: '0px 0px 188px -14px rgba(237,255,32,1)',
+        testShadow: '11px 0px 13px -15px rgba(0,0,0,1)',
       },
     },
   },
-  plugins: [require("tailwind-scrollbar")],
-};
+  plugins: [require('tailwind-scrollbar')],
+}
